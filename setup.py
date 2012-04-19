bc2b00bda2e6540f35d93f6190b0b0e7b4c45349
Guido A.J. Stevens <guido.stevens@cosent.nl>
2012-04-19 12:53:42 +0200

    initial checkin from ZopeSkel

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='plonesocial.activitystream',
+      version=version,
+      description="This package provides views for plonesocial.microblog status updates and other user activities in Plone",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='plone socbiz social microblog activity stream',
+      author='Guido Stevens',
+      author_email='guido.stevens@cosent.net',
+      url='http://github.com/cosent/plonesocial.activitystream',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['plonesocial'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={'test': ['plone.app.testing']},
+      entry_points="""
+      # -*- Entry points: -*-
+  	  [z3c.autoinclude.plugin]
+  	  target = plone
+      """,
+      )
