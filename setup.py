881d394d00e6e420b45989c76bcfe16d1e30c9d4
Guido A.J. Stevens <guido.stevens@cosent.nl>
2013-04-29 08:17:10 +0000

    Prepare plonesocial.activitystream 0.4.3.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.4.2'
+version = '0.4.3'
 
 long_description = (
     open('README.rst').read()
@@ -16,13 +16,14 @@ long_description = (
 setup(
     name='plonesocial.activitystream',
     version=version,
-    description="This package provides views for plonesocial.microblog status updates and other user activities in Plone",
+    description=("This package provides views for "
+                 "plonesocial.microblog status updates and other "
+                 "user activities in Plone"),
     long_description=long_description,
     # Get more strings from
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        "Programming Language :: Python",
-        ],
+        "Programming Language :: Python", ],
     keywords='plone socbiz social microblog activity stream',
     author='Guido Stevens',
     author_email='guido.stevens@cosent.net',
@@ -34,12 +35,11 @@ setup(
     zip_safe=False,
     install_requires=[
         'setuptools',
-        'Plone',
-        ],
+        'Plone', ],
     extras_require={'test': ['plone.app.testing']},
     entry_points="""
       # -*- Entry points: -*-
-  	  [z3c.autoinclude.plugin]
-  	  target = plone
+          [z3c.autoinclude.plugin]
+          target = plone
       """,
-    )
+)
